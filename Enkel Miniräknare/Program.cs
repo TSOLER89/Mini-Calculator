bd56250eb5058be7e76b6a155e22ca8ed7eb4f7d
Tsoler Hayitian <tsoler.hayitian@gmail.com>
2025-09-15 11:39:54 +0200

    Add project files.

---
+++ Enkel Miniräknare/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Enkel_Miniräknare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välja en operation:");
+        }
+    }
+}
